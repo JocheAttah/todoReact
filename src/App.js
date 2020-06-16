e184bc392e86721740c2da72c71c85bd4a5f01ab
Attah David 'Joche <39128641+JocheAttah@users.noreply.github.com>
2020-06-16 14:19:08 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import "./styles.css"; 
+import TodoForm from "./components/TodoForm"
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  function addTodo(todo){
+    setTodos([todo, ...todos]); 
+
+  }
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>React Todo</p>
+        <TodoForm addTodo= { addTodo }/>
+      </header>
+       
+    </div>
+  );
+}
+
+export default  App;
